8c16496e99616501f787401dbe47796b036a4fe2
vaysol <vaysol72@gmail.com>
2023-07-22 00:57:56 +0530

    Design Push 1

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -30,6 +30,12 @@ $routes->set404Override();
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
+$routes->get('/about', 'Home::about');
+$routes->get('/services', 'Home::services');
+$routes->get('/team', 'Home::team');
+$routes->get('/contact-us', 'Home::contact_us');
+
+
 
 /*
  * --------------------------------------------------------------------
---
+++ app/Views/footer.php
@@ -0,0 +1,101 @@
+  <!-- ======= Footer ======= -->
+  <footer id="footer" class="footer">
+
+    <div class="footer-newsletter">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-12 text-center">
+            <h4>Our Newsletter</h4>
+            <p>B2B Marketing News and Updates from Vaysol</p>
+          </div>
+          <div class="col-lg-6">
+            <form action="forms/sub.php" method="post">
+              <input type="email" name="email"><input type="submit" value="Subscribe">
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row gy-4">
+          <div class="col-lg-5 col-md-12 footer-info">
+            <a href="index.html" class="logo d-flex align-items-center">
+              <img src="<?php echo base_url(); ?>assets/img/icon/logo.png" alt="">
+              <span>Vaysol</span>
+            </a>
+            <p>Vaysol is a team of experienced marketers who specialize in B2B marketing. We offer a range of services, including lead generation, demand generation, and content marketing, to help our clients grow their businesses.
+            </p>
+            <div class="social-links mt-3">
+              <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+              <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+              <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+              <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+            </div>
+          </div>
+
+          <div class="col-lg-2 col-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><i class="bi bi-chevron-right"></i> <a href="#hero">Home</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="#about">About us</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="#services">Services</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="#team">Team</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="#contact">Contact</a></li>
+              <!-- <li><i class="bi bi-chevron-right"></i> <a href="#">Terms of service</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="#">Privacy policy</a></li> -->
+            </ul>
+          </div>
+
+          <div class="col-lg-2 col-6 footer-links">
+            <h4>Our Services</h4>
+            <ul>
+              <li><i class="bi bi-chevron-right"></i> <a href="<?php echo base_url('/services') ?>">Custom Website Development</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="<?php echo base_url('/services') ?>">E-commerce Development</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="<?php echo base_url('/services') ?>">Content Management System</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="<?php echo base_url('/services') ?>">Search Engine Optimization (SEO)</a></li>
+             
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
+            <h4>Contact Us</h4>
+            <p>A-301, SRS Appartments<br>10<sup>th</sup> Cross Road, JP Nagar Phase 1<br>Bangalore 560078 <br>
+            <!-- <strong>Phone:</strong> +1 5589 55488 55<br> -->
+              <strong>Email:</strong> info@vaysol.in<br>
+            </p>
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>Vaysol</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        Designed by <a href="https://vaysol.in/">Vaysol Technologies</a> &amp; <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="<?php echo base_url(); ?>assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="<?php echo base_url(); ?>assets/vendor/aos/aos.js"></script>
+  <script src="<?php echo base_url(); ?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="<?php echo base_url(); ?>assets/js/main.js"></script>
+
+</body>
+
+</html>
